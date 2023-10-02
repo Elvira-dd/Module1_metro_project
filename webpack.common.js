74cd24725a71cd550f88518d97225a7aa84c945d
Elvira Galieva <elvira.6.galieva@yandex.ru>
2023-10-02 14:10:45 +0300

    Init commit

---
+++ webpack.common.js
@@ -0,0 +1,138 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPartialsPlugin = require('html-webpack-partials-plugin')
+
+const webpack = require('webpack')
+const path = require('path')
+
+module.exports = {
+  entry: {
+    index: './src/index.js'
+  },
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'docs')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+            plugins: ['@babel/plugin-proposal-class-properties']
+          }
+        }
+      },
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true
+          }
+        }
+      },
+      {
+        test: /\.scss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [['postcss-preset-env']]
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader'
+      },
+      {
+        resourceQuery: /raw/,
+        type: 'asset/source'
+      },
+      {
+        test: /\.png/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[hash][ext][query]'
+        }
+      },
+      {
+        test: /\.svg/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[hash][ext][query]'
+        }
+      },
+      {
+        test: /\.(ttf|otf|woff|woff2)$/i,
+        loader: 'file-loader',
+        options: {
+          name: 'fonts/[name].[ext]'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+      chunkFilename: '[id].[contenthash].css'
+    }),
+
+    // Index
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: './index.html'
+    }),
+    //Section Страницы разделов
+    new HtmlWebpackPlugin({
+      template: './src/chronology.html',
+      filename: './chronology.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/catalog.html',
+      filename: './catalog.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/facts.html',
+      filename: './facts.html'
+    }),
+
+    // Article Статьи
+    //Подключение новых html страниц
+    new HtmlWebpackPlugin({
+      template: './src/articles/revolution.html',
+      filename: './articles/revolution.html'
+    }),
+
+    // Partials
+    //Мы можем указать файл откуда можно вытащить информацию и поместить этот блок куда угодно
+    new HtmlWebpackPartialsPlugin([
+      {
+        path: path.join(__dirname, './src/partials/menu.html'),
+        location: 'menu',
+        //Название кусочка аналитика
+        template_filename: '*',
+        priority: 'replace'
+      }
+    ])
+  ],
+  optimization: {
+    minimizer: [new CssMinimizerPlugin()]
+  }
+}
